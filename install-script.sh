2e04229832d2e4b4d7119ff8f7b58a0d8b9ac23f
Brad <brdthmpson@gmail.com>
2017-01-29 19:10:47 -0800

    fixed homebrew issue, now neutral
    - Homebrew is now installed within the script (yay)
    - Any user should be able to run this. The script will be called using sudo but some commands will be run without root
    - Mac configurations are now (kind of) in the same spot. Ideally these would be in separate files. This is okay with me for now.

--- install-script.sh
+++ install-script.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/env bash
 
-#Set up log environment
-LOG_FILE='/Users/brad/Desktop/install-log.log'
+#TODO Make sure computer doesn't sleep.
+
+#Set up logging environment
+LOG_FILE='/Users/'$(logname)'/Desktop/install-log.log'
 
 exec &> >(tee -a "$LOG_FILE")
 echo "Logging to" $LOG_FILE
 
-#PREREQ CHECK - Homebrew installed
-if [ "$(which brew)" != "/usr/local/bin/brew" ]; then
-  echo "FAILED PREREQ CHECK - Homebrew must be installed before this script can run. This may be fixed later. Exiting...."
-  exit 1
-fi
-
-#Install Xcode Tools
-echo "Installing Xcode Tools"
-xcode-select --install
-
-#Homebrew must be installed prior to installation because we do it automatically
-#TODO Make sure computer doesn't sleep.
-
 #Set Hostname
-hostname CastleByers
+hostname CastleByers #I like Stranger Things don't judge me.
 echo "Host name is set to $(hostname)" #TODO Make sure this actually sticks after restart
 
 #Mac configuration
+echo "Setting up computer configuration"
 systemsetup -setcomputersleep 15 #Computer sleeps after 15 minutes
 defaults write com.apple.screensaver askForPassword 1 #Force password entry after sleep
+
+echo "Installing Xcode Tools" #Might need this so that git commands work.
+xcode-select --install
+
+$SCREENSHOT_DIRECTORY="/Users/'$(logname)'/Desktop/Screenshots"
+if [ -d "$SCREENSHOT_DIRECTORY" ]; then
+  echo "Setting up screenshot folder"
+  cd ~/Desktop && mkdir Screenshots
+fi
+
+echo "Setting up screenshot configuration..."
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
 
 #Check if FileValut is on
 if [ "$(fdesetup status)" == "FileVault is On." ]; then
@@ -37,39 +39,35 @@ if [ "$(fdesetup status)" == "FileVault is Off." ]; then
   fdesetup enable
 fi
 
-#Change Screenshot Location to Screenshots Folder on Desktop
-$SCREENSHOT_DIRECTORY="/Users/brad/Desktop/Screenshots"
-if [ -d "$SCREENSHOT_DIRECTORY" ]; then
-  echo "Setting up screenshot folder"
-  cd ~/Desktop && mkdir Screenshots
-fi
-
-echo "Setting up screenshot configuration..."
-defaults write com.apple.screencapture location ~/Desktop/Screenshots
+#Setup Brew
+sudo -u $SUDO_USER /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #Setup Software
-echo "Installing cask software ionstall.. (may take long if the internet is slow)"
+echo "Installing software from cask.. (may take long if the internet is slow)"
 cask_apps="atom discord dropbox spotify telegram 1password iterm2 sonos stremio transmission-nightly vlc-nightly google-chrome vmware-fusion"
 
-su brad -c "brew install mas" #We will need this laster for installing MAS
-su brad -c "brew tap caskroom/cask"
-su brad -c "brew tap caskroom/versions"
-su brad -c "brew cask install Caskroom/cask/$cask_apps"
+sudo -u $SUDO_USER brew install mas #We will need this laster for installing MAS
+sudo -u $SUDO_USER brew tap caskroom/cask
+sudo -u $SUDO_USER brew tap caskroom/versions
+sudo -u $SUDO_USER brew cask install Caskroom/cask/$cask_apps
+sudo -u $SUDO_USER brew install wget #Needed for Google Drive links
 
 #Install Mac Store Apps
-
 if mas account; then
     echo "Installing Mac Store Apps..."
     mac_apps_ids="425424353 1088330492 409789998"
-    su brad -c "mas install $mac_apps_ids"
+    sudo -u $SUDO_USER mas install $mac_apps_ids
 else
     echo "Not signed in to MAS. You'll need to install them manually for now :("
 fi
 
 #TODO Download and move non-casked apps: Flux
 
+#TODO Mac Software Update Check and install updates
+
 #Just some cleanup
 echo "Resetting UI for Screenshot default folder to take effect"
 killall SystemUIServer
 
-echo "all done!:) 🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏 BLESS UP!"
+#WE OUT
+echo "all done!:) 🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏"
