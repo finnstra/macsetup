d7577f60c7c3e626a54c8eb0f524d509668f1b17
Brad Thompson <brad@Brads-MacBook-Pro.local>
2017-10-01 09:35:20 -0700

    Comment cleanup, removed airsonos, added/removed apps

--- install-script.sh
+++ install-script.sh
@@ -1,33 +1,36 @@
 #!/usr/bin/env bash
 
-#TODO Make sure computer doesn't sleep.
-
-#Set up logging environment
+# Set up logging environment
 LOG_FILE='/Users/'$(logname)'/Desktop/install-log.log'
 
 exec &> >(tee -a "$LOG_FILE")
 echo "Logging to" $LOG_FILE
 
-#TODO Set computer name and ensure it is set properly. 
+# TODO Set computer name and ensure it is set properly. 
 
-#Mac configuration
+# Mac configuration
 echo "Setting up computer configuration"
 systemsetup -setcomputersleep 15 #Computer sleeps after 15 minutes
+systemsetup -setdisplaysleep 15 #Computer display sleeps after 15 minutes
+# TODO: Figure out how to set different times for battery vs. power adapter
+
 defaults write com.apple.screensaver askForPassword 1 #Force password entry after sleep
-sudo spctl --master-disable #Disables 'gatekeeper'
+sudo spctl --master-disable #Disables 'gatekeeper.' You don't have to do this if you don't rely on it.
 
 SCREENSHOT_DIRECTORY="/Users/$(logname)/Desktop/Screenshots"
 if [ ! -d "$SCREENSHOT_DIRECTORY" ]; then
   echo "Setting up screenshot folder"
-  cd ~/Desktop && mkdir $SCREENSHOT_DIRECTORY
+  cd ~/Desktop
+  sudo -u mkdir $SCREENSHOT_DIRECTORY #Doing this because I've ran into permissions issues before
 fi
 
 echo "Setting up screenshot configuration..."
-defaults write com.apple.screencapture location ~/Desktop/Screenshots/
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
 
-#Check if FileValut is on
+
+# Check if FileValut is on
 if [ "$(fdesetup status)" == "FileVault is On." ]; then
-  echo "Disk encryption is lit. 🔥"
+  echo "Disk encryption is enabled. 🔥"
 fi
 
 if [ "$(fdesetup status)" == "FileVault is Off." ]; then
@@ -35,12 +38,12 @@ if [ "$(fdesetup status)" == "FileVault is Off." ]; then
   fdesetup enable
 fi
 
-#Setup Brew
+# Setup Brew
 sudo -u $SUDO_USER /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#Setup Software
+# Setup Software
 echo "Installing software from cask.. (may take long if the internet is slow)"
-cask_apps="atom skitch discord dropbox spotify telegram 1password iterm2 sonos stremio transmission-nightly vlc-nightly google-chrome vmware-fusion"
+cask_apps="visual-studio-code skitch discord dropbox telegram 1password hyperterm sonos stremio transmission-nightly vlc-nightly google-chrome"
 
 sudo -u $SUDO_USER brew install mas #We will need this laster for installing MAS
 sudo -u $SUDO_USER brew tap caskroom/cask
@@ -48,24 +51,16 @@ sudo -u $SUDO_USER brew tap caskroom/versions
 sudo -u $SUDO_USER brew cask install Caskroom/cask/$cask_apps
 sudo -u $SUDO_USER brew install wget #Needed for Google Drive links
 
-#Install Mac Store Apps
+# Install Mac Store Apps
 open "/Applications/App Store.app";
-read -p "Ensure you are signed in to the App Store, then hit enter. ";
+read -p "Ensure you are signed in to the App Store with your Apple ID, then hit enter. ";
 echo "Installing Mac Store Apps..."
 mac_apps_ids="425424353 1088330492 409789998 668208984 634148309"
 sudo -u $SUDO_USER mas install $mac_apps_ids
 
-#Install Airsonos (for my Sonos)
-sudo -u $SUDO_USER brew install npm
-sudo -u $SUDO_USER npm install airsonos
-sudo -u $SUDO_USER npm install -g n
-
-#Switch Node to 0.10.33 for compatability with airsonos
-n 0.10.33
-
-#Just some cleanup
+# Cleanup
 echo "Resetting UI for screenshot default settings to take effect"
 killall SystemUIServer
 
-#WE OUT
+# Done :)
 echo "all done!:) 🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏🙏"
